c42287303cd2f537b567bd62d78030e44e2c7e71
Daniel Pan <daniel.pan@inmobi.com>
2019-02-26 18:48:49 -0800

    Work on Venue and styling

--- src/index.js
+++ src/index.js
@@ -45,6 +45,7 @@ class Menu extends React.Component {
       <BrowserRouter>
         <div>
           <Route path='/' component={Home} exact />
+          <Route path='/venue' component={VenueList} exact />
           <Route path="/user/:username" component={User}/>
           <Route path='/equipment' component={EquipmentTable} exact />
           <Route path='/equipment/:id' component={EquipmentItem} exact />
@@ -60,7 +61,209 @@ class Menu extends React.Component {
   }  
 }
 
-// class Menu extends React.Component {
+class VenueList extends React.Component {
+  renderVenueList() {
+    return (
+      VenueItemList.map(
+        ( _venue, _index ) => (
+          <Venue key={ _index }
+          venue={ _venue} 
+          />
+      ) )
+    )
+  }  
+  render () {
+    return (
+      <div className="venue-list">
+        <div className="card-columns">
+          {this.renderVenueList()}
+        </div>
+      </div>
+    );
+  }
+}
+    // return (
+    //   <div className="venue card">
+    //     <div className="card-img">
+    //       <img src={'images/' + this.props.venue.picture_url} />
+    //     </div>
+    //     <div className="card-img-overlay d-flex flex-column justify-content-end">
+    //       <div className="d-flex">
+    //         <div className="description flex-grow-1">
+    //           <p className="card-text title">{this.props.venue.name}</p>
+    //           <p className="card-text location">{this.props.venue.location.name}, {this.props.venue.location.city}, {this.props.venue.location.country}</p>
+    //           <p className="card-text date">{this.props.venue.date}</p>
+    //         </div>
+    //         <div className="actions">
+    //           <i className="far fa-share-square"></i>
+    //           <i className="far fa-heart"></i>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // )
+
+class Venue extends React.Component {
+  render () {
+    return (
+      <div className="card card-cascade wider">
+
+        <div className="view view-cascade overlay">
+          <img className="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/photo6.jpg" />
+          <a className="#!">
+            <div className="mask rgba-white-slight"></div>
+          </a>
+        </div>
+
+        <div className="card-body card-body-cascade text-center">
+          <h4 className="card-title"><strong>Alison Belmont</strong></h4>
+          <h5 className="blue-text pb-2"><strong>Graffiti Artist</strong></h5>
+          <p className="card-text">Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque laudantium, totam rem aperiam. </p>
+        </div>
+      </div>
+    )
+  };
+}
+
+var VenueItemList = [
+  {
+    id: 1,
+    name: 'Venue 1',
+    location: {
+      address: '',
+      name: 'Globe Theatre',
+      city: 'Vancouver',
+      country: 'Canada',
+    },
+    time: '5:00pm',
+    date: '02/14/19',
+    picture_url: 'pexels-photo-92646.jpg',
+    price: 50,
+    description: 'This is a description',
+  },
+  {
+    id: 2,
+    name: 'Venue 2',
+    location: {
+      address: '',
+      name: 'Globe Theatre',
+      city: 'Sydney',
+      country: 'Austrailia',
+    },
+    time: '5:00pm',
+    date: '02/14/19',
+    picture_url: 'people-show-chairs-gym.jpg',
+    price: 50,
+    description: 'This is a description',
+  },
+  {
+    id: 3,
+    name: 'Venue 3',
+    location: {
+      address: '',
+      name: 'Globe Theatre',
+      city: 'Taipei',
+      country: 'Taiwan',
+    },
+    time: '5:00pm',
+    date: '02/14/2019',
+    picture_url: 'pexels-photo-159524.jpg',
+    price: 50,
+    description: 'This is a description',
+  },
+  {
+    id: 4,
+    name: 'Venue 4',
+    location: {
+      address: '',
+      name: 'Globe Theatre',
+      city: 'Ashville',
+      country: 'North Carolina',
+    },
+    time: '5:00pm',
+    date: '02/14/2019',
+    picture_url: 'pexels-photo-270085.jpg',
+    price: 50,
+    description: 'This is a description',
+  },
+  {
+    id: 5,
+    name: 'Venue 5',
+    location: {
+      address: '',
+      name: 'Globe Theatre',
+      city: 'Miami',
+      country: 'Florida',
+    },
+    time: '5:00pm',
+    date: '02/14/2019',
+    picture_url: 'pexels-photo-722562.jpg',
+    price: 50,
+    description: 'This is a description',
+  },
+  {
+    id: 6,
+    name: 'Venue 6',
+    location: {
+      address: '',
+      name: 'Globe Theatre',
+      city: 'Austin',
+      country: 'Texas',
+    },
+    time: '5:00pm',
+    date: '02/14/2019',
+    picture_url: 'pexels-photo-1035665.jpg',
+    price: 50,
+    description: 'This is a description',
+  },
+  {
+    id: 7,
+    name: 'Venue 7',
+    location: {
+      address: '',
+      name: 'Globe Theatre',
+      city: 'Portland',
+      country: 'Oregon',
+    },
+    time: '5:00pm',
+    date: '02/14/2019',
+    picture_url: 'pexels-photo-1578317.jpg',
+    price: 50,
+    description: 'This is a description',
+  },
+  {
+    id: 8,
+    name: 'Venue 8',
+    location: {
+      address: '',
+      name: 'Globe Theatre',
+      city: 'New York',
+      country: 'California',
+    },
+    time: '5:00pm',
+    date: '02/14/2019',
+    picture_url: 'pexels-photo-1683292.jpg',
+    price: 50,
+    description: 'This is a description',
+  },
+  {
+    id: 9,
+    name: 'Venue 9',
+    location: {
+      address: '',
+      name: 'Globe Theatre',
+      city: 'Los Angeles',
+      country: 'United States',
+    },
+    time: '5:00pm',
+    date: '02/14/2019',
+    picture_url: 'pexels-photo-1713080.jpg',
+    price: 50,
+    description: 'This is a description',
+  }
+];
+          
+          
 //   render () {
 //     return (
 //       <BrowserRouter>
